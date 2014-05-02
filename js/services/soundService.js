482c75e7e4bd043df016d364987bbfc543ae90a7
olivier <olivier.texier@gmail.com>
2014-05-02 22:41:52 +0200

    otx

--- js/services/soundService.js
+++ js/services/soundService.js
@@ -7,12 +7,17 @@ agbeServices.factory('soundService', ['$log',function ($log) {
         },
 
         play : function(name) {
-            var auOgg = new Audio('./media/'+name+".ogg");
+            /*var auOgg = new Audio('./media/'+name+".ogg");
             auOgg.load();
             auOgg.play();
             var auMp3 = new Audio('./media/'+name+".mp3");
             auMp3.load();
             auMp3.play();
+            */
+            var dodo = new Media('/android_asset/www/media/sword.mp3', function(e) {alert('succes : '+e)}, function(e) {alert('erreur : '+e)});
+            dodo.play();
+            var dodo2 = new Media('/android_asset/www/media/sword.ogg', function(e) {alert('succes : '+e)}, function(e) {alert('erreur : '+e)});
+            dodo2.play();
         }
 
     }
